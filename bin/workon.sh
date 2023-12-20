da3713a39268954828215932b3bea31ae2317f1a
konsougiou <ks2220@ic.ac.uk>
2023-12-20 10:53:41 +0000

    setup

---
+++ bin/workon.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJ_ROOT=${THIS_DIR}/..
+
+pushd ${PROJ_ROOT} >> /dev/null
+
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+if [ ! -d "venv" ]; then
+    ./bin/create_venv.sh
+fi
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    source venv/bin/activate
+    export PS1="(nydus-experiment) $PS1"
+fi
+
+# Update submodules
+git submodule update --init
+
+# Invoke tab-completion
+_complete_invoke() {
+    local candidates
+    candidates=`invoke --complete -- ${COMP_WORDS[*]}`
+    COMPREPLY=( $(compgen -W "${candidates}" -- $2) )
+}
+
+# If running from zsh, run autoload for tab completion
+if [ "$(ps -o comm= -p $$)" = "zsh" ]; then
+    autoload bashcompinit
+    bashcompinit
+fi
+complete -F _complete_invoke -o default invoke inv
+
+# Pick up project-specific binaries
+export PATH=${PROJ_ROOT}/bin:${PATH}
+
+
+popd >> /dev/null
